9edf66c7e36e0c63d617b9ae60339c1605c00c98
master <mail@dehuaio.com>
2019-02-12 15:45:40 +0800

    fonticon

--- miniprogram/component/navbar/navbar.js
+++ miniprogram/component/navbar/navbar.js
@@ -9,18 +9,39 @@ Component({
     title: {
       type: String,
       value: '极客宝箱',
+    },
+    nav: {
+      type: Object,
+      value: null,
+      observer: function(newVal, oldVal, changedPath) {
+        var navs = this.data.navs;
+        navs[2] = newVal;
+        this.setData({
+          navs,
+        });
+      }
     }
   },
   data: {
     height: 60,
-    showBack: true,
     showCapsule: true,
-    title: "极客宝箱"
+    title: "极客宝箱",
+    navs: [{
+      type: "navigateBack",
+      show: true,
+      text: "\ue011"
+    }, {
+      type: "switchTab",
+      url: '/pages/index/index',
+      show: true,
+      text: "\ue00b",
+      color: '#f70'
+    }]
   },
   attached: function() {
     this.setData({
       height: wx.getMenuButtonBoundingClientRect().bottom + 10,
-      showBack: getCurrentPages().length != 1,
+      "navs[0].show": getCurrentPages().length > 1
     })
   },
   methods: {}
