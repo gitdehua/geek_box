38f27c2d3d5664be09fd756130f39a139201f05d
master <mail@dehuaio.com>
2019-04-09 14:32:35 +0800

    AccessToken

--- cloudfunctions/syncAccessToken/index.js
+++ cloudfunctions/syncAccessToken/index.js
@@ -1,23 +1,40 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk');
 var request = require('request');
-var crypto = require('crypto');
+var mysql = require('mysql');
 
 cloud.init();
 const db = cloud.database();
 
 // 云函数入口函数
 exports.main = async(event, context) => new Promise((resolve, reject) => {
-  var appid = "wx05e2e4dade4a9b04";
-  var secret = "5630052487f02952c44a5d64424252fd";
-  var url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${secret}`;
+  var url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${process.env.appid}&secret=${process.env.secret}`;
 
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // 请求成功的处理逻辑
       try {
         body = JSON.parse(body);
-        syncDB(body).then(() => {
+        var connection = mysql.createConnection({
+          host: process.env.db_host,
+          user: process.env.db_user,
+          password: process.env.db_password,
+          database: process.env.db_database
+        });
+
+        connection.connect();
+
+        var modSql = "UPDATE `wxapp` SET `value` = ?, `update_time` = NOW() WHERE `key` = 'accessToken';";
+        var modSqlParams = [body.access_token];
+        
+        connection.query(modSql, modSqlParams, function(err, result) {
+          if (err) {
+            reject('[UPDATE ERROR] - ', err.message);
+            return;
+          }
+          console.log('--------------------------UPDATE----------------------------');
+          console.log('UPDATE affectedRows', result.affectedRows);
+          console.log('-----------------------------------------------------------------\n\n');
           db.collection('app_config').doc('AccessToken').update({
             data: {
               value: body.access_token
@@ -25,12 +42,12 @@ exports.main = async(event, context) => new Promise((resolve, reject) => {
           }).then(function() {
             resolve(body);
           })
-        })
+        });
       } catch (e) {
-        resolve(e);
+        reject(e);
       }
     } else {
-      resolve({
+      reject({
         error,
         response,
         body
@@ -38,30 +55,3 @@ exports.main = async(event, context) => new Promise((resolve, reject) => {
     }
   });
 });
-
-function syncDB(data) {
-  return new Promise((resolve, reject) => {
-    try {
-      var md5 = crypto.createHash('md5');
-      request({
-        url: "https://www.dehuaio.com/api/UpdateAccessToken.php",
-        method: "POST",
-        json: true,
-        headers: {
-          "content-type": "application/json",
-          "content-md5": md5.update(JSON.stringify(data)).digest('hex')
-        },
-        body: data
-      }, function(error, response, body) {
-        console.log(error, body);
-        resolve({
-          error,
-          response,
-          body
-        })
-      });
-    } catch (err) {
-      console.log("try", err);
-    }
-  });
-}
