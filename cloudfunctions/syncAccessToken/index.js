7ad63949e9b77aa00e7089c06c9dfae4fb3b1e6c
master <mail@dehuaio.com>
2019-09-06 00:05:29 +0800

    MySQL

--- cloudfunctions/syncAccessToken/index.js
+++ cloudfunctions/syncAccessToken/index.js
@@ -1,6 +1,7 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk');
 var request = require('request');
+var mysql = require('mysql');
 
 cloud.init();
 const db = cloud.database();
@@ -12,15 +13,46 @@ exports.main = async(event, context) => new Promise((resolve, reject) => {
   request(url, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       console.log(body) // 请求成功的处理逻辑
+
+      var statuts = 0;
+
+      function _return() {
+        if (statuts == 1) {
+          resolve(body);
+        } else {
+          statuts++;
+        }
+      }
       try {
         body = JSON.parse(body);
         db.collection('app_config').doc('AccessToken').update({
           data: {
             value: body.access_token
           }
-        }).then(function () {
-          resolve(body);
-        })
+        }).then(function() {
+          _return("cloud_db");
+        });
+
+        var connection = mysql.createConnection({
+          host: process.env.dbHost,
+          user: process.env.dbUser,
+          password: process.env.dbPassword,
+          database: process.env.dbDatabase
+        });
+
+        connection.connect();
+
+        var modSql = "UPDATE `wxapp` SET `value` = ?, `update_time` = NOW() WHERE `key` = 'accessToken';";
+        var modSqlParams = [body.access_token];
+
+        connection.query(modSql, modSqlParams, function(err, result) {
+          if (err) {
+            reject('[UPDATE ERROR] - ', err.message);
+            return;
+          }
+          console.log(`--------------------------UPDATE----------------------------\nUPDATE affectedRows ${result.affectedRows}\n-----------------------------------------------------------------\n\n`);
+          _return("mysql");
+        });
       } catch (e) {
         reject(e);
       }
