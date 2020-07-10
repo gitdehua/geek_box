25aa29c420592f9163c83d6099ea6168b4f533ca
master <hidehua@outlook.com>
2020-07-10 18:05:16 +0800

    cloudflare api

--- miniprogram/pages/cloudflare/selectZone.js
+++ miniprogram/pages/cloudflare/selectZone.js
@@ -5,17 +5,46 @@ Page({
    * 页面的初始数据
    */
   data: {
+    slideButtons: [{
+      type: 'warn',
+      text: '删除'
+    }],
+  },
 
+  slideButtonTap(e) {
+    console.log(e)
+    switch (e.detail.index) {
+      case 0: // 删除
+        this.removeZone(e.target.dataset.zoneId);
+        break;
+    }
+  },
+
+  removeZone: function (id) {
+    let zones = this.data.zones;
+    delete zones[id]
+    wx.setStorage({
+      data: zones,
+      key: "zones",
+      success: () => {
+        wx.showToast({
+          title: "删除成功",
+        });
+        this.setData({
+          zones
+        });
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
     var _this = this;
     wx.getStorage({
       key: 'zones',
-      success: function(res) {
+      success: function (res) {
         _this.setData({
           zones: res.data
         })
@@ -23,13 +52,13 @@ Page({
     })
   },
 
-  navToBindToken: function(e) {
+  navToBindToken: function (e) {
     console.log(e);
     var _this = this;
     wx.navigateTo({
       url: "./bindToken",
       events: {
-        getZones: function(data) {
+        getZones: function (data) {
           _this.setData({
             zones: data
           });
